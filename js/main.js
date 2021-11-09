adc279bb4a294a177cde98b4efc542511a99d888
pibolib <jonoschool953@gmail.com>
2021-11-08 22:23:49 -0500

    mobile compat test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var target = '18%';
 window.onload = function() {
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        var target = '100%';
+        var root = document.querySelector(':root');
+        root.style.setProperty('--sidebar-width', '0%');
+    }
     var a = document.getElementById('sidebarbutton');
     a.onclick = function() {
         var root = document.querySelector(':root');
