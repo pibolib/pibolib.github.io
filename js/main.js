73519b4444f3b28430a08635e00613e5080c1ab9
pibolib <jonoschool953@gmail.com>
2021-11-08 22:33:27 -0500

    mobile compatibility

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 var target = '18%';
 window.onload = function() {
     if (/Mobi|Android/i.test(navigator.userAgent)) {
-        var target = '100%';
+        target = '100%';
         var root = document.querySelector(':root');
         root.style.setProperty('--sidebar-width', '0%');
-        var textA = document.getElementsByClassName('sidebar a');
-        textA.style.setProperty('font-size','40pt');
-        var textP = document.getElementsByClassName('sidebar p');
-        textP.style.setProperty('font-size','40pt');
     }
     var a = document.getElementById('sidebarbutton');
     a.onclick = function() {
