b4c7e7c4b118ee36713265e0a42af7b9d4d52bb0
pibolib <jonoschool953@gmail.com>
2021-11-16 11:25:14 -0500

    attempt for better mobile look and transitions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-var target = '18%';
+var target = '-18%';
 window.onload = function() {
     if (/Mobi|Android/i.test(navigator.userAgent)) {
-        target = '100%';
+        target = '-100%';
         var root = document.querySelector(':root');
-        root.style.setProperty('--sidebar-width', '0%');
+        root.style.setProperty('--sidebar-width', '-100%');
+        root.style.setProperty('--sidebar-width-absolute','100%');
+        root.style.setProperty('--sidebar-text-size', '5vh');
     }
     var a = document.getElementById('sidebarbutton');
     a.onclick = function() {
